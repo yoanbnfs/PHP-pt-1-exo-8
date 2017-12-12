c3862cfe00c6285f28500cdbdf02cc4f3dda435a
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-12-12 14:18:02 +0100

    exercice

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo-8</title>
+  </head>
+  <body>
+    <?php
+      $addition = 3 + 4;
+      $multiplication = 5 * 20;
+      $division = 45 / 5;
+      echo $addition . ' ' . $multiplication . ' ' . $division; 
+    ?>
+  </body>
+</html>
